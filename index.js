b5cb999369ff31aa5e4a649f64258bfdfc9a1e8f
ARYAN <48391385+thearyanag@users.noreply.github.com>
2022-05-22 09:31:45 +0530

    middleware

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const dotenv = require('dotenv').config()
+
+require('./db/connect');
+
+const app = express();
+app.use(helmet());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('combined'));
